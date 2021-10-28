049094d48a63fcd6b88c49bc092791c0b0da8b9c
qqbit <pavlik.merkulov@gmail.com>
2021-10-28 11:20:19 +0300

    test

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -1,16 +1,18 @@
-const router = require('express').Router();
+const router = require("express").Router();
 // models db
-const {
-  Order, Request, Customer, Example, Product, Quantity,
-} = require('../db/models');
+const { Order, Request, Customer, Example, Product, Quantity } = require("../db/models");
 
-router.get('/', async (req, res) => {});
+router.get("/", async (req, res) => {});
 
-router.get('/orders', async (req, res) => {});
+router.get("/orders", async (req, res) => {});
 
-router.get('/products', async (req, res) => {});
+router.route("/products").get(async (req, res) => {
+  const products = await Product.findAll();
+  console.log(products);
+  res.render("adminProduct", { products });
+});
 
-router.get('/requests', async (req, res) => {});
+router.get("/requests", async (req, res) => {});
 
-router.get('/customers', async (req, res) => {});
+router.get("/customers", async (req, res) => {});
 module.exports = router;
